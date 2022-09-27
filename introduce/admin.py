ca3be5293e0964b169aa5aa680a81f0ad95e716e
HAERIM00 <kaerim4@gmail.com>
2022-09-27 13:31:06 +0900

    쪽지시험

--- introduce/admin.py
+++ introduce/admin.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
 from .models import AcceessLog
 
-# Register your models here.
 admin.site.register(AcceessLog) 
--- introduce/models.py
+++ introduce/models.py
@@ -1,9 +1,14 @@
 #user/models.py
 from django.db import models
 
-class AcceessLog(models.Model):
-    class Meta:
-        db_table = "my_user"
-
-    created_at = models.DateTimeField(auto_now_add=True)
-    location = models.CharField(max_length=64)
+class AccessLog(models.Model):
+    """
+    1. default : 기본적으로 사용될 날짜를 사용자가 지정
+    2. auto_now : 데이터가 수정 될 때마다 갱신됨
+    3. auto_now_add : 데이터가 생성 될 때 시간을 기록
+    """
+    created_at = models.DateTimeField("접속 시간", auto_now_add=True)
+    location = models.CharField("접속 경로", max_length=50)
+    
+    def __str__(self):
+        return f"{self.created_at} / {self.location}"
