559c43a72310b2c4cd12a63cbddeec4759847a61
HAERIM00 <kaerim4@gmail.com>
2022-09-27 09:56:31 +0900

    sparta

--- introduce/models.py
+++ introduce/models.py
@@ -1,3 +1,9 @@
+#user/models.py
 from django.db import models
 
-# Create your models here.
+class AcceessLog(models.Model):
+    class Meta:
+        db_table = "my_user"
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    location = models.CharField(max_length=64)
